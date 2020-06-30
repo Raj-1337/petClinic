b8bf9fb3f937ab40048bd2e0b4cf976c2beba985
Raj Kumar <srk1998007@gmail.com>
2020-06-30 21:24:15 +0530

    refactored service interfaces.

--- pet-clinic-data/src/main/java/works/wckd/petClinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/works/wckd/petClinic/services/OwnerService.java
@@ -2,15 +2,8 @@ package works.wckd.petClinic.services;
 
 import works.wckd.petClinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lname);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
