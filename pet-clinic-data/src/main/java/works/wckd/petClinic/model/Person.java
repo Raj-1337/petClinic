e5f0bb55a65d3976fc1342786b3823ab07c1aa5f
Raj Kumar <srk1998007@gmail.com>
2020-06-30 16:57:36 +0530

    Adding BaseEntity with property ID. closes #10.

--- pet-clinic-data/src/main/java/works/wckd/petClinic/model/Person.java
+++ pet-clinic-data/src/main/java/works/wckd/petClinic/model/Person.java
@@ -1,6 +1,6 @@
 package works.wckd.petClinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/works/wckd/petClinic/model/PetType.java
+++ pet-clinic-data/src/main/java/works/wckd/petClinic/model/PetType.java
@@ -1,6 +1,6 @@
 package works.wckd.petClinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
